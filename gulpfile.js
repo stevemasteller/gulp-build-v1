7380bc49335831c9f2d2aa0e4ce2d075235ab236
stevemasteller <stevermasteller@gmail.com>
2016-10-05 12:51:53 -0400

    Added comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
+/**********************************************************
+*
+*  gulpfile.js
+*
+* Implements a gulp build for development and deployment. 
+* The process does the following:
+*	- Concatenates and minifies the JavaScript files.
+*	- Compiles SCSS into CSS, concatenates and minifies.
+*	- Generates JavaScript and CSS source maps.
+*	- Compresses JPEG and PNG files.
+* Output for development is in the home directory structure 
+* gulp-build-v1. Output for deployment or distribution is 
+* in the dist folder at gulp-buil-v1/dist.
+*
+* Author: Steve Masteller
+*
+************************************************************/
 'use strict';
 
 var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
- cssnano = require('gulp-cssnano'),
-	maps = require('gulp-sourcemaps'),
-imagemin = require('gulp-imagemin'),
-	 del = require('del'),
-  useref = require('gulp-useref'),
-  eslint = require('gulp-eslint');
+  concat = require('gulp-concat'),		// concatenate files
+  uglify = require('gulp-uglify'),		// minify JavaScript
+  rename = require('gulp-rename'),		// rename a file
+    sass = require('gulp-sass'),		// compile SAss
+ cssnano = require('gulp-cssnano'),		// minify CSS
+	maps = require('gulp-sourcemaps'),	// create source maps
+imagemin = require('gulp-imagemin'),	// minify images
+	 del = require('del'),				// delete directories and files
+  useref = require('gulp-useref'),		// replace css and js references in html
+  eslint = require('gulp-eslint');		// lint
 
+// Concatenate the .js sources into a single file
+// map it, and put it in js/global.js. js/global.js 
+// is used for development.
 gulp.task('concatScripts', function() {
 	return gulp.src([
 		'js/jquery-3.1.1.js',
@@ -23,6 +43,9 @@ gulp.task('concatScripts', function() {
 	.pipe(gulp.dest('js'));
 });
 
+// Lint global.js, minify it and store the result
+// in dist/scripts/all.min.js.  all.min.js is used
+// for deployment.
 gulp.task('scripts', ['concatScripts'], function() {
 	return gulp.src(['js/global.js'])
 	.pipe(eslint())
@@ -33,6 +56,9 @@ gulp.task('scripts', ['concatScripts'], function() {
 	.pipe(gulp.dest('dist/scripts'));
 });
 
+// Compile sass/global.scss, map it, and write the
+// result to css/global.css.  global.css is used for
+// development. 
 gulp.task('compileSass', function() {
 	return gulp.src(['sass/global.scss'])
 	.pipe(maps.init())
@@ -41,6 +67,9 @@ gulp.task('compileSass', function() {
 	.pipe(gulp.dest('css'));
 });
 
+// Minify css/global.css, and write the result to
+// dist/styles/all.min.css.  all.min.css is used for
+// deployment.
 gulp.task('styles', ['compileSass'], function() {
 	return gulp.src(['css/global.css'])
 	.pipe(cssnano())
@@ -48,18 +77,26 @@ gulp.task('styles', ['compileSass'], function() {
 	.pipe(gulp.dest('dist/styles'));
 });
 
+// Minify the images in images/* and write the 
+// results to dist/images/*.  These minified images
+// are used for deployment.
 gulp.task('images', function() {
 	return gulp.src('images/*')
 	.pipe(imagemin())
 	.pipe(gulp.dest('dist/images'));
 });
 
+// Take the index.html file used for development and
+// replace the .css and .js file calls with the versions 
+// used for deployment. Store the resulting index.html file
+// in the dist folder.
 gulp.task('html', ['scripts', 'styles'], function() {
 	return gulp.src('index.html')
 	.pipe(useref())
 	.pipe(gulp.dest('dist'));
 });
 
+// Remove all generated files and folders.
 gulp.task('clean', function() {
 	return del([
 		'dist',
@@ -67,16 +104,21 @@ gulp.task('clean', function() {
 		'js/global.js*']);
 });
 
+// Recompiles sass and js files when they change.
 gulp.task('watchFiles', function() {
   gulp.watch(['sass/*.scss', 'sass/**/*.sass'], ['styles']);	
   gulp.watch(['js/circle/*.js'], ['scripts']);
 });
 
+// full build for development and deployment
 gulp.task('build', ['clean'], function() {
 	return gulp.start(['html', 'images']);
 });
 
-// http-server -p 3000
+// serves watchFiles when a local server is
+// running like:
+// 		http-server -p 3000
 gulp.task('serve', ['watchFiles'])
 
+// defualts 'gulp build' to just 'gulp'
 gulp.task('default', ['build']);
