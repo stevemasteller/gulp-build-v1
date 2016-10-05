21cab9084119f5016c6d132b64285e97a7de2d6f
stevemasteller <stevermasteller@gmail.com>
2016-10-05 06:09:20 -0400

    Compile scripts and styles
    ignore generated files in .gitignore
    _utilities.sass wouldn't compile to css, commented out the offending
    media queries.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,38 @@
 'use strict';
 
 var gulp = require('gulp'),
-  concat = require('gulp-concat');
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+ cssnano = require('gulp-cssnano');
 
-gulp.task("concatScripts", function() {
+gulp.task('concatScripts', function() {
 	gulp.src([
 		'js/circle/autogrow.js',
 		'js/circle/circle.js'])
 	.pipe(concat('global.js'))
 	.pipe(gulp.dest('js'));
+});
+
+gulp.task('scripts', function() {
+	gulp.src(['js/global.js'])
+	.pipe(uglify())
+	.pipe(rename('all.min.js'))
+	.pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('compileSass', function() {
+	gulp.src(['sass/global.scss'])
+	.pipe(sass())
+	.pipe(gulp.dest('css'));
+});
+
+gulp.task('styles', function() {
+	gulp.src(['css/global.css'])
+	.pipe(cssnano())
+	.pipe(rename('all.min.css'))
+	.pipe(gulp.dest('dist/styles'));
 });
 
 gulp.task('default', ['hello'], function() {
